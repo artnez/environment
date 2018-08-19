8681430c63ea78b53d87bf1df557822443c102e2
Artem Nezvigin <artem@artnez.com>
2018-08-19 12:24:31 -0700

    vim: Update command-t

--- packages/vim/dot/vim/bundle/commandt/spec/command-t/scanner/file_scanner_spec.rb
+++ packages/vim/dot/vim/bundle/commandt/spec/command-t/scanner/file_scanner_spec.rb
@@ -1,13 +1,12 @@
-# Copyright 2010-2014 Greg Hurrell. All rights reserved.
+# Copyright 2010-present Greg Hurrell. All rights reserved.
 # Licensed under the terms of the BSD 2-clause license.
 
 require 'spec_helper'
-require 'command-t/scanner/file_scanner'
 
-describe CommandT::FileScanner do
+describe CommandT::Scanner::FileScanner do
   before do
     dir = File.join(File.dirname(__FILE__), '..', '..', '..', 'fixtures')
-    @scanner = CommandT::FileScanner.new(dir)
+    @scanner = CommandT::Scanner::FileScanner.new(dir)
   end
 
   describe 'flush method' do
